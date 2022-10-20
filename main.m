e176e787c8299b28a4fbc7bf94371c61de0b0bb1
YANYU ZHANG <zhangya@bu.edu>
2022-10-19 20:27:49 -0700

    Add files via upload

--- main.m
+++ main.m
@@ -9,5 +9,16 @@ clear all;
 close all;
 clc;
 
-data = double(readmatrix('V1_01_easy.txt'));
-plot(data(:, 2), data(:, 3));
+[data_T, data] = load_simulated_trajectory('V1_01_easy.txt');
+
+% imu measurement: imu.am, imu.wm, imu.timestamp
+
+featmap = simulator(data_T);
+
+for i = 1:1:length(featmap.')
+    uv_map(i, 1:3) = featmap(i).p_FinG;
+end
+
+plot3(data(:, 2), data(:, 3), data(:, 4));
+hold on;
+plot3(uv_map(:,1), uv_map(:,2), uv_map(:,2), '.')
